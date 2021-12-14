4a2d362c71d370aa09bab41652ede2a4c00ad9fe
carlos738 <carlosasl135@hotmail.com>
2021-12-14 16:09:02 -0300

    PROJECTS

--- springbootconfig3/delombok/src-bak/src/main/java/com/digitalinoovationone/springboot3/config/Setter.java
+++
@@ -1,4 +0,0 @@
-package com.digitalinoovationone.springboot3.config;
-
-public @interface Setter {
-}
--- springbootconfig3/src/main/java/com/digitalinoovationone/springboot3/config/Setter.java
+++
@@ -1,2 +0,0 @@
-package com.digitalinoovationone.springboot3.config;public @interface Setter {
-}
